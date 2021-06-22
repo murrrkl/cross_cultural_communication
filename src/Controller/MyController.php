2ec9d251d9fefebcb7c1b64cb76fc2c6a45d37b5
murrrkl <murkl@vm151781.local>
2021-06-22 09:45:15 +0300

    add

--- src/Controller/MyController.php
+++ src/Controller/MyController.php
@@ -3,13 +3,14 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MyController extends AbstractController
 {
     /**
-     * @Route("/my", name="my")
+     * @Route("/my", name="my", methods={"GET"})
      */
     public function index(): Response
     {
@@ -25,9 +26,15 @@ class MyController extends AbstractController
     public function search(Request $request)
     {
 
-        $a = $request->request->getText('a');
+        $a = $request->request->get('a');
+        /*file_put_contents('test.txt', $a."\n", 8);*/
         $fp = fopen("test.txt", "w");
         fwrite($fp, $a);
         fclose($fp);
+        echo $a;
+        file_put_contents('test.txt', $a."\n", 8);
+        return $this->render('my/index.html.twig', [
+            'text' => $a,
+        ]);
     }
 }
